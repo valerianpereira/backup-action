8e1421d6d4a108ff716a650152555b40139ac1af
Darshan Gada <gadadarshan@gmail.com>
2020-08-22 15:39:39 +0530

    Test if this works

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,26 @@ set -eu
 
 export GITHUB="true"
 
-sh -c "/bin/drone-ssh $*"
+THEDATE=`date +%d%m%y%H%M`
+
+# Check what to be back up
+echo "Backup type: $type"
+if [ "$TYPE" = "db" ]
+  then
+    FILENAME=mysql-$DB_NAME.$THEDATE.sql.gz
+    if [ "$DB_TYPE" = "mysql" ]
+      then
+        SCRIPT="mysqldump -q -u $DB_USER -p'$DB_USER' $DB_NAME | gzip -9 > $FILENAME"
+    fi
+fi
+
+# Execute SSH Commands to create backups first
+sh -c "/bin/drone-ssh $*" 
+
+# Load the deploy key
+mkdir -p ~/.ssh && echo $DEPLOY_KEY > ~/.ssh/deploy_key && chmod 600 ~/.ssh/deploy_key
+
+# Rsync the backup files to container
+rsync --remove-source-files -avzhe 'ssh -i ~/.ssh/deploy_key -o StrictHostKeyChecking=no -p 22' --progress $USERNAME@$HOST:./mysql* ./backup/
+
+ls -lha
