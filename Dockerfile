b1312f1c549d5ba6757775b31759e7a95fc52ee4
Darshan Gada <gadadarshan@gmail.com>
2020-09-11 18:42:57 +0530

    Backing up Mongo (#2)

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,7 @@
 FROM appleboy/drone-ssh:1.6.2-linux-amd64
 
 # Install rsync
-RUN apk --update add --no-cache rsync openssh-client git dpkg \
-	&& apk add hub --repository=http://dl-cdn.alpinelinux.org/alpine/v3.3/community \
-	&& rm -rf /var/cache/apk/*
-
-RUN hub version
-
-# Install github-cli
-# RUN wget https://github.com/cli/cli/releases/download/v0.11.1/gh_0.11.1_linux_amd64.deb
-# RUN dpkg -i *.deb
+RUN apk --update add --no-cache rsync openssh-client git dpkg && rm -rf /var/cache/apk/*
 
 ADD entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,30 @@
 
 set -eu
 
+# Required for appleboy/ssh-action
 export GITHUB="true"
 
 THEDATE=`date +%d%m%y%H%M`
+BACKUP_DIR="backups"
 
 # Check what to be back up
 echo "Backup type: $INPUT_TYPE"
 if [ "$INPUT_TYPE" = "db" ]
   then
-    FILENAME=mysql-$INPUT_DB_NAME.$THEDATE.sql.gz
     echo "DB type: $INPUT_DB_TYPE"
     if [ "$INPUT_DB_TYPE" = "mysql" ]
       then
-        INPUT_SCRIPT="mysqldump -q -u $INPUT_DB_USER -p'$INPUT_DB_PASS' $INPUT_DB_NAME | gzip -9 > $FILENAME"
+        FILENAME=mysql-$INPUT_DB_NAME.$THEDATE.sql.gz
+        INPUT_DB_PORT="${INPUT_DB_PORT:-3306}"
+        INPUT_SCRIPT="mysqldump -q -u $INPUT_DB_USER -P $INPUT_DB_PORT -p'$INPUT_DB_PASS' $INPUT_DB_NAME | gzip -9 > $FILENAME"
+    fi
+
+    if [ "$INPUT_DB_TYPE" = "mongo" ]
+      then
+        FILENAME=mongo-$INPUT_DB_NAME.$THEDATE.gz
+        INPUT_DB_PORT="${INPUT_DB_PORT:-27017}"
+        INPUT_AUTH_DB="${INPUT_AUTH_DB:-admin}"
+        INPUT_SCRIPT="mongodump --port=$INPUT_DB_PORT -d $INPUT_DB_NAME -u $INPUT_DB_USER -p='$INPUT_DB_PASS' --authenticationDatabase=$INPUT_AUTH_DB --gzip -o backmon && tar -cvzf $FILENAME backmon/$INPUT_DB_NAME"
     fi
 fi
 
@@ -33,29 +44,27 @@ echo "Done!!"
 #-----------------------------
 # CREATE DESTINATION DIR IF NOT EXISTS
 #-----------------------------
-if [ ! -d ./$INPUT_DB_NAME/ ]
-then
-    mkdir $INPUT_DB_NAME
+if [ ! -d ./$BACKUP_DIR/ ]
+  then
+    mkdir $BACKUP_DIR
 fi
 
 echo "Show me destination dir.."
 ls -la
 
 # Rsync the backup files to container
-echo "Sync the backups..."
-echo "Run command: rsync --remove-source-files -avzhe 'ssh -i $HOME/.ssh/deploykey -o StrictHostKeyChecking=no -p 22' --progress $INPUT_USERNAME@$INPUT_HOST:./mysql* ./$INPUT_DB_NAME/"
+echo "Sync the $INPUT_DB_TYPE backups..."
+if [ "$INPUT_DB_TYPE" = "mysql" ]
+  then
+    sh -c "rsync --remove-source-files -avzhe 'ssh -i $HOME/.ssh/deploykey -o StrictHostKeyChecking=no' --progress $INPUT_USERNAME@$INPUT_HOST:./mysql* ./$BACKUP_DIR/"
+fi
 
-sh -c "rsync --remove-source-files -avzhe 'ssh -i $HOME/.ssh/deploykey -o StrictHostKeyChecking=no' --progress $INPUT_USERNAME@$INPUT_HOST:./mysql* ./$INPUT_DB_NAME/"
+if [ "$INPUT_DB_TYPE" = "mongo" ]
+  then
+    sh -c "rsync --remove-source-files -avzhe 'ssh -i $HOME/.ssh/deploykey -o StrictHostKeyChecking=no' --progress $INPUT_USERNAME@$INPUT_HOST:./mongo* ./$BACKUP_DIR/"
+fi
+
 
 echo "Show me backups..."
-ls -la && ls -l ./$INPUT_DB_NAME/
+ls -la && ls -l ./$BACKUP_DIR/
 
-echo "Create a branch & Raise PR"
-git switch -c backup-action-files
-git add $INPUT_DB_NAME/
-
-git -c user.name="valerianpereira" -c user.email="valerianpereira25@gmail.com" \
-    commit -m "Add backup files" \
-    --author="Valerian Pereira <valerianpereira25@gmail.com>"
-
-git push --set-upstream origin backup-action-files
